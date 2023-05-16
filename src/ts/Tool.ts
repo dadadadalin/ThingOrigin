8523c6d3c3d4d021e8897b456c8e696320e875aa
gj <gj2751439488@163.com>
2023-05-16 14:57:34 +0800

    获取模型顶点数面数

--- src/ts/Tool.ts
+++ src/ts/Tool.ts
@@ -1,5 +1,6 @@
-import { Box3, Color, Object3D, Sphere, Vector3 } from "three";
+import { Box3, Color, Object3D, Sphere, Vector3, Mesh, Geometry, BufferGeometry } from "three";
 import { CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer";
+import {ThingOrigin} from "../ThingOrigin";
 
 /**
  * 自定义工具方法
@@ -149,6 +150,110 @@ export class Tool {
 
         return info;
     }
+  /**
+   * @description 获取模型顶点数
+   * @author gj
+   * @date 2023/5/16
+   * @param {Object3D} model 模型
+   * @returns {*}  {number} 顶点数
+   */
+  public getModelVerticesCount(model: Object3D):number {
+    if (!model) {
+      console.warn("获取信息失败，物体不存在");
+      return;
+    }
+    let vertices = 0; //模型顶点
+    model.traverseVisible(function (object) {
+      if (object instanceof Mesh) {
+        let geometry = object.geometry;
+        if (geometry instanceof Geometry) {
+          vertices += geometry.vertices.length;
+        } else if (geometry instanceof BufferGeometry && geometry.attributes.position) {
+          vertices += geometry.attributes.position.count;
+        }
+      }
+    });
+    return vertices;
+  }
+
+  /**
+   * @description 获取模型三角面数
+   * @author gj
+   * @date 2023/5/16
+   * @param {Object3D} model 模型
+   * @returns {*}  {number} 三角面数
+   */
+  public getModelFaceCount(model: Object3D):number {
+    if (!model) {
+      console.warn("获取信息失败，物体不存在");
+      return;
+    }
+    let triangles  = 0; //模型面数
+    model.traverseVisible(function (object) {
+      if (object instanceof Mesh) {
+        let geometry = object.geometry;
+        if (geometry instanceof Geometry) {
+          triangles += geometry.faces.length;
+        } else if (geometry instanceof BufferGeometry && geometry.attributes.position) {
+          if (geometry.index !== null) {
+            triangles += geometry.index.count / 3;
+          } else {
+            triangles += geometry.attributes.position.count / 3;
+          }
+        }
+      }
+    });
+    return triangles;
+  }
+
+  /**
+   * @description 模型全屏预览
+   * @author gj
+   * @date 2023/5/16
+   * @param {Object3D} model 模型
+   */
+  public fullScreen(model: Object3D):void {
+    if (!model) {
+      console.warn("获取信息失败，物体不存在");
+      return;
+    }
+
+    let box = this.getObjectBox(model);
+    var v3 = new Vector3();
+    // 获得包围盒长宽高尺寸，结果保存在参数三维向量对象v3中
+    box.getSize(v3);
+
+    // 计算包围盒的最大边长
+    function num() {
+      var max;
+      if (v3.x > v3.y) {
+        max = v3.x
+      } else {
+        max = v3.y
+      }
+      if (max > v3.z) {
+      } else {
+        max = v3.z
+      }
+      return max;
+    }
+
+    // 根据最大边长设置缩放倍数，尽量全屏显示
+    // 150：参照相机正投影相机参数left、right、top和bottom设置一个合适的值
+    // 过大会超出屏幕范围，过小全屏效果不明显
+    var S = 150 / num();
+    // 对模型进行缩放操作，实现全屏效果
+    model.scale.set(S, S, S);
+
+    // // 重新计算包围盒，重新计算包围盒，不能使用原来的包围盒必须重新声明一个包围盒
+    // let newBox3 = this.getObjectBox(target);
+    // var center = new Vector3() // 计算一个层级模型对应包围盒的几何体中心
+    // newBox3.getCenter(center)
+    // // 重新设置模型的位置，使模型居中
+    // model.position.x = model.position.x - center.x;
+    // model.position.y = model.position.y - center.y;
+    // model.position.z = model.position.z - center.z;
+  }
 
     /**
      * @description 判断模型是否有2D元素
